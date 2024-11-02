cd11e8eae39a15b1f97e92bc5cb1535779213079
Ventax <maulanakenang24@gmail.com>
2024-11-02 21:07:39 +0700

    Game ABC

---
+++ script.js
@@ -0,0 +1,140 @@
+const sound1 = document.querySelector('#sound1');
+const sound2 = document.querySelector('#sound2');
+const sound3 = document.querySelector('#sound3');
+const sound4 = document.querySelector('#sound4');
+const sound5 = document.querySelector('#sound5');
+const sound6 = document.querySelector('#sound6');
+const sound7 = document.querySelector('#sound7');
+const sound8 = document.querySelector('#sound8');
+const sound9 = document.querySelector('#sound9');
+const sound10 = document.querySelector('#sound10');
+const sound11 = document.querySelector('#sound11');
+const sound12 = document.querySelector('#sound12');
+const sound13 = document.querySelector('#sound13');
+const sound14 = document.querySelector('#sound14');
+const sound15 = document.querySelector('#sound15');
+const sound16 = document.querySelector('#sound16');
+const sound17 = document.querySelector('#sound17');
+const sound18 = document.querySelector('#sound18');
+const sound19 = document.querySelector('#sound19');
+const sound20 = document.querySelector('#sound20');
+const sound21 = document.querySelector('#sound21');
+const sound22 = document.querySelector('#sound22');
+const sound23 = document.querySelector('#sound23');
+const sound24 = document.querySelector('#sound24');
+const sound25 = document.querySelector('#sound25');
+const sound26 = document.querySelector('#sound26');
+
+function playSound(number){
+    if(typeof number == 'number'){
+        stopSound();
+        if(number == 1){
+            sound1.play();
+        }else if(number == 2){
+            sound2.play();
+        }else if(number == 3){
+            sound3.play();
+        }else if(number == 4){
+            sound4.play();
+        }else if(number == 5){
+            sound5.play();
+        }else if(number == 6){
+            sound6.play();
+        }else if(number == 7){
+            sound7.play();
+        }else if(number == 8){
+            sound8.play();
+        }else if(number == 9){
+            sound9.play();
+        }else if(number == 10){
+            sound10.play();
+        }else if(number == 11){
+            sound11.play();
+        }else if(number == 12){
+            sound12.play();
+        }else if(number == 13){
+            sound13.play();
+        }else if(number == 14){
+            sound14.play();
+        }else if(number == 15){
+            sound15.play();
+        }else if(number == 16){
+            sound16.play();
+        }else if(number == 17){
+            sound17.play();
+        }else if(number == 18){
+            sound18.play();
+        }else if(number == 19){
+            sound19.play();
+        }else if(number == 20){
+            sound20.play();
+        }else if(number == 21){
+            sound21.play();
+        }else if(number == 22){
+            sound22.play();
+        }else if(number == 23){
+            sound23.play();
+        }else if(number == 24){
+            sound24.play();
+        }else if(number == 25){
+            sound25.play();
+        }else if(number == 26){
+            sound26.play();
+        }
+    }
+}
+
+function stopSound(){
+    sound1.pause();
+    sound1.currentTime = 0;
+    sound2.pause();
+    sound2.currentTime = 0;
+    sound3.pause();
+    sound3.currentTime = 0;
+    sound4.pause();
+    sound4.currentTime = 0;
+    sound5.pause();
+    sound5.currentTime = 0;
+    sound6.pause();
+    sound6.currentTime = 0;
+    sound7.pause();
+    sound7.currentTime = 0;
+    sound8.pause();
+    sound8.currentTime = 0;
+    sound9.pause();
+    sound9.currentTime = 0;
+    sound10.pause();
+    sound10.currentTime = 0;
+    sound11.pause();
+    sound11.currentTime = 0;
+    sound12.pause();
+    sound12.currentTime = 0;
+    sound13.pause();
+    sound13.currentTime = 0;
+    sound14.pause();
+    sound14.currentTime = 0;
+    sound15.pause();
+    sound15.currentTime = 0;
+    sound16.pause();
+    sound16.currentTime = 0;
+    sound17.pause();
+    sound17.currentTime = 0;
+    sound18.pause();
+    sound18.currentTime = 0;
+    sound19.pause();
+    sound19.currentTime = 0;
+    sound20.pause();
+    sound20.currentTime = 0;
+    sound21.pause();
+    sound21.currentTime = 0;
+    sound22.pause();
+    sound22.currentTime = 0;
+    sound23.pause();
+    sound23.currentTime = 0;
+    sound24.pause();
+    sound24.currentTime = 0;
+    sound25.pause();
+    sound25.currentTime = 0;
+    sound26.pause();
+    sound26.currentTime = 0;
+}
